7e8d00189b013678f1ab7dc9d2d227fd655272d9
NickGroenl <bartocanitrot1@hotmail.com>
2022-09-30 12:31:51 -0300

    Files tests

--- test/app_test.js
+++ test/app_test.js
@@ -1,16 +1,29 @@
-var expect  = require('chai').expect;
-var request = require('request');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
 
+chai.use(chaiHttp);
+chai.should();
+
+var app = require('../app');
 describe('Main App', function() {
-    
-
-    describe ('About page', function() {
-        it('status', function(done){
-            request('http://localhost:8080/example', function(error, response, body) {
-                expect(!!response?.statusCode).to.equal(202);
-                done();
+    describe('API /v1/files/data', () => {
+        it('it should return 202', (done) => {
+          chai.request(app)
+            .get('/v1/files/data')
+            .end((err, res) => {
+              res.should.have.status(202);
+              done();
             });
         });
-
+    });
+    describe('API /v1/files/data/select', () => {
+        it('it should return 202', (done) => {
+            chai.request(app)
+                .post('/v1/files/data/select').send({filename: 'test9.csv'})
+                .end((err, res) => {
+                    res.should.have.status(202);
+                    done();
+                });
+        });
     });
 });
